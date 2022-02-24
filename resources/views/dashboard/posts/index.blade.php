185212664766131f1bb217c04662254212c8bc29
AlYgmi <nurrusulialadiyat@gmail.com>
2022-02-24 11:25:01 +0700

    lanjutan

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -30,8 +30,12 @@
           <td>{{ $post->category->nama }}</td>
           <td>
             <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
-            <a href="" class="badge bg-warning"><span data-feather="edit"></span></a>
-            <a href="" class="badge bg-danger"><span data-feather="trash"></span></a>
+            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+            <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+              @method('delete')
+              @csrf
+              <button class="badge bg-danger border-0" onclick="return confirm('Are you sure want to delete?')"><span data-feather="trash"></span></button>
+            </form>
           </td>
         </tr>
       @endforeach
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -7,8 +7,12 @@
             <h1 class="mb-3">{{ $post->title }}</h1>
 
             <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to All My Post's</a>
-            <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit My Post's</a>
-            <a href="" class="btn btn-danger"><span data-feather="trash"></span> Delete</a>
+            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit My Post's</a>
+            <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+                @method('delete')
+                @csrf
+                <button class="btn btn-danger" onclick="return confirm('Are you sure want to delete?')"><span data-feather="trash"></span> Delete</button>
+            </form>
 
             <img src="https://source.unsplash.com/1200x400?{{ $post->category->nama }}" alt="{{ $post->category->nama }}" class="img-fluid mt-3">
             <article class="my-3 fs-5">
